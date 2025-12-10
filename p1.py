85f9551d9bb528df2c9afe14d6f34711e56fbf90
Nammi Jaswanth <jaswanthnammi145@gmail.com>
2025-12-10 22:30:44 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,6 @@
+
+name=input("What is your name? ")
+age=int(input("How old are you? "))
+hobby=input("What is your favorite hobby? ")
+print(f"Welcome {name}!")
+print(f"You are {age} years old and love {hobby}.")
